b383939139f63d4933ea9b860361e257f52536d6
Tore Stensaker Tefre <tore@tefre.it>
2019-08-16 21:19:32 +0200

    added initial CV data

--- src/containers/Cv/index.js
+++ src/containers/Cv/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 export const Cv = () => {
-  return <h1>CV</h1>;
+  return (
+    <article>
+      <h1>CV</h1>
+      <h2>Tore Stensaker Tefre</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Periode</th>
+            <th>Organisasjon</th>
+            <th>Aktivitet</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Sommer 2019</td>
+            <td>Oslo Market Solutions / Oslo Børs VPS</td>
+            <td>React-utvikling</td>
+          </tr>
+        </tbody>
+      </table>
+    </article>
+  );
 };
 
 export default Cv;
