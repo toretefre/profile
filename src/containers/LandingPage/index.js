dc6926bab2e8261e9f5a66299bd8d69c28a289fe
Tore Stensaker Tefre <tore@tefre.it>
2019-08-16 21:08:53 +0200

    added link to cv on landingpage

--- src/containers/LandingPage/index.js
+++ src/containers/LandingPage/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from '@reach/router';
 
 export const LandingPage = () => {
-  return <h1>LandingPage</h1>;
+  return (
+    <main>
+      <h1>Tore Stensaker Tefre</h1>
+
+      <Link to="/cv">CV</Link>
+    </main>
+  );
 };
 
 export default LandingPage;
