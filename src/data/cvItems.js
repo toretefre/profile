1939eaa07104049102a894fa1e66eb7fe5e8b4fe
Tore Stensaker Tefre <tore@tefre.it>
2019-08-17 17:57:26 +0200

    added education

--- src/data/cvItems.js
+++ src/data/cvItems.js
@@ -2,6 +2,31 @@ import React, { Fragment } from 'react';
 
 export const cvItems = [
   {
+    title: 'Informatikk - master',
+    employer: 'NTNU Gløshaugen',
+    location: 'Trondheim',
+    period: 'August 2019 - Juni 2021',
+    description: (
+      <Fragment>
+        <p>Interaksjonsdesign, spill- og læringsteknologi</p>
+        <p>Tverrfaglig prosjektarbeid</p>
+      </Fragment>
+    )
+  },
+  {
+    title: 'Informatikk - bachelor',
+    employer: 'NTNU Gløshaugen',
+    location: 'Trondheim',
+    period: 'August 2016 - Juni 2019',
+    description: (
+      <Fragment>
+        <p>Systemutvikling, webutvikling, og datavitenskap</p>
+        <p>Prosjektarbeid med smidig arbeidsmetodikk</p>
+        <p>Webutvikling med toppkarakter</p>
+      </Fragment>
+    )
+  },
+  {
     title: 'Frontendutvikler',
     employer: 'Oslo Market Solutions / Oslo Børs',
     location: 'Oslo',
@@ -19,6 +44,19 @@ export const cvItems = [
     employer: 'NTNU IT',
     location: 'Trondheim',
     period: 'Oktober 2016 - d.d.',
+    description: (
+      <Fragment>
+        <p>Frontendutvikler</p>
+        <p>Jobbet med Arena</p>
+        <p>Teamet var fire personer</p>
+      </Fragment>
+    )
+  },
+  {
+    title: 'Sommerstudent',
+    employer: 'Huawei Technologies',
+    location: 'Shenzhen, Kina',
+    period: 'Juli 2018',
     description: (
       <Fragment>
         <p>Frontendutvikler</p>
