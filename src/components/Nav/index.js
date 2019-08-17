685d3cb61020390a895367d9d332e77ec0c9f2a3
Tore Stensaker Tefre <tore@tefre.it>
2019-08-17 15:34:30 +0200

    supporting external links

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import links from './../../data/links';
 
 export const Nav = () => {
   return (
     <nav>
       <ul>
-        <li>
-          <Link to="/">Hjem</Link>
-        </li>
-        <li>
-          <Link to="/cv">CV</Link>
-        </li>
+        {links
+          .filter(item => !item.url.includes('http'))
+          .map(item => (
+            <li>
+              <Link to={item.url}>{item.title}</Link>
+            </li>
+          ))}
+        {links
+          .filter(item => item.url.includes('http'))
+          .map(item => (
+            <li>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
       </ul>
     </nav>
   );
---
+++ src/data/links.js
@@ -0,0 +1,7 @@
+export const links = [
+  { url: '/', title: 'Hjem', type: 'internal' },
+  { url: '/cv', title: 'CV', type: 'internal' },
+  { url: 'https://linkedin.com/in/tore', title: 'LinkedIn' }
+];
+
+export default links;
