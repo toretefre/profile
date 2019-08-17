1b4631bc4e729d7ff2fec283ad6f104280290747
Tore Stensaker Tefre <tore@tefre.it>
2019-08-17 14:21:31 +0200

    added some responsiveness

--- src/containers/LandingPage/index.js
+++ src/containers/LandingPage/index.js
@@ -3,11 +3,19 @@ import React from 'react';
 export const LandingPage = () => {
   return (
     <article>
+      <p className="centertext">
+        Informatikkstudent, utvikler, DJ, og bordtennis-amatør!
+      </p>
       <img
         src="https://www.adressa.no/incoming/article15292846.ece/gjlmps/ALTERNATES/w980-default/doc6wna0n8vti8l6jiyedt.jpg"
         alt="Tore med PC"
       />
-      <p className="centertext">Alltid på jobb, legg merke til PC-en ;)</p>
+      <p className="centertext">
+        En god utvikler har alltid med seg PC{' '}
+        <span role="img" alt="blunkefjes" aria-label="blunkefjes">
+          😉
+        </span>
+      </p>
     </article>
   );
 };
